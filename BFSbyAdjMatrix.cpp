94403db8b8594327f27486e1e407306e46d1e3fa
DhananjayKumar28 <106928047+DhananjayKumar28@users.noreply.github.com>
2022-06-11 11:37:47 +0530

    Add files via upload

---
+++ BFSbyAdjMatrix.cpp
@@ -0,0 +1,56 @@
+#include <queue>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printBFS(int** edges, int n, int sv){
+    queue<int> pendingVertices;
+    bool *visited =new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+    pendingVertices.push(sv);
+    visited[sv]=true;
+    while(!pendingVertices.empty()){
+        int currentVertex=pendingVertices.front();
+        pendingVertices.pop();
+        cout<<currentVertex<<endl;
+        for(int i=0;i<n;i++){
+            if(i==currentVertex){
+                continue;
+            }
+            if(edges[currentVertex][i]==1 && !visited[i]){
+                pendingVertices.push(i);
+                visited[i];
+            }
+        }
+    }
+    delete [] visited;
+}
+
+int main(){
+    int n;
+    int e;
+    cin>>n>>e;
+    int **edges=new int*[n];
+    for(int i=0;i<n;i++){
+        edges[i]=new int [n];
+        for(int j=0;j<n;j++){
+            edges[i][j]=0;
+        }
+    }
+    for(int i=0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edges[f][s]=1;
+        edges[s][f]=1;
+    }
+
+    bool* visited=new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i]=false;
+    }
+
+    delete [] visited;
+    cout<<"BFS"<<endl;
+    printBFS(edges,n,0);
+}
